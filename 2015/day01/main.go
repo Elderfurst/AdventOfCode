e0d0780bd1ed9868f543bd82cf08918ff9a602e1
Nick Anderson <njcanderson@gmail.com>
2024-10-18 15:17:19 -0400

    Add solutions for day 01 2015

--- 2015/day01/main.go
+++ 2015/day01/main.go
@@ -1,1 +1,29 @@
 package main
+
+import (
+	"adventofcode/utilities"
+	"fmt"
+)
+
+func main() {
+	inputCharacters := utilities.ReadInputFileToCharacters("input.txt")
+
+	currentFloor := 0
+	firstBasement := true
+
+	for index, character := range inputCharacters {
+		switch character {
+		case "(":
+			currentFloor++
+		case ")":
+			currentFloor--
+		}
+
+		if currentFloor < 0 && firstBasement == true {
+			fmt.Println(index + 1)
+			firstBasement = false
+		}
+	}
+
+	fmt.Println(currentFloor)
+}
