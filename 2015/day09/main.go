096b32bb3f4a02f1a1f838ed71383e3d6be83962
Nick Anderson <njcanderson@gmail.com>
2024-11-03 14:39:22 -0500

    Add 2015 day 09 final solutions

--- 2015/day09/main.go
+++ 2015/day09/main.go
@@ -3,9 +3,7 @@ package main
 import (
 	"adventofcode/utilities"
 	"fmt"
-	"github.com/hmdsefi/gograph"
-	"github.com/hmdsefi/gograph/traverse"
-	"math"
+	"slices"
 	"strconv"
 	"strings"
 )
@@ -19,56 +17,107 @@ func partOne() {
 	inputLines := utilities.ReadInputFileToLines("2015/day09/input.txt")
 	graph := parseInputLines(inputLines)
 
+	paths := make([]int, 0)
+	for node := range graph.nodes {
+		visited := make(map[string]bool)
+		paths = append(paths, getShortestPath(graph, node, visited))
+	}
+
+	fmt.Println(slices.Min(paths))
 }
 
 func partTwo() {
+	inputLines := utilities.ReadInputFileToLines("2015/day09/input.txt")
+	graph := parseInputLines(inputLines)
 
+	paths := make([]int, 0)
+	for node := range graph.nodes {
+		visited := make(map[string]bool)
+		paths = append(paths, getLongestPath(graph, node, visited))
+	}
+
+	fmt.Println(slices.Max(paths))
+}
+
+func getShortestPath(graph *Graph, node string, visited map[string]bool) int {
+	visited[node] = true
+
+	if len(visited) == len(graph.nodes) {
+		return 0
+	}
+
+	paths := make([]int, 0)
+	for _, edge := range graph.edges[node] {
+		if !visited[edge.right] {
+			paths = append(paths, edge.distance+getShortestPath(graph, edge.right, copyVisited(visited)))
+		}
+	}
+
+	return slices.Min(paths)
+}
+
+func getLongestPath(graph *Graph, node string, visited map[string]bool) int {
+	visited[node] = true
+
+	if len(visited) == len(graph.nodes) {
+		return 0
+	}
+
+	paths := make([]int, 0)
+	for _, edge := range graph.edges[node] {
+		if !visited[edge.right] {
+			paths = append(paths, edge.distance+getLongestPath(graph, edge.right, copyVisited(visited)))
+		}
+	}
+
+	return slices.Max(paths)
 }
 
 func parseInputLines(inputLines []string) *Graph {
-	graph := &Graph {
-		Nodes: make([]*Node, 0),
-		Edges: make([]*Edge, 0),
+	graph := &Graph{
+		nodes: make(map[string]bool),
+		edges: make(map[string][]*Edge),
 	}
 
 	for _, line := range inputLines {
 		splitLine := strings.Split(line, " ")
 		first := splitLine[0]
 		second := splitLine[2]
-		distance, _ := strconv.ParseFloat(splitLine[4], 64)
+		distance, _ := strconv.Atoi(splitLine[4])
 
-		if graph
+		graph.nodes[first] = true
+		graph.nodes[second] = true
 
-		firstNode := new Node {
-			Label: first,
-		}
+		graph.edges[first] = append(graph.edges[first], &Edge{
+			left:     first,
+			right:    second,
+			distance: distance,
+		})
+		graph.edges[second] = append(graph.edges[second], &Edge{
+			left:     second,
+			right:    first,
+			distance: distance,
+		})
 	}
 
 	return graph
 }
 
-type Graph struct {
-	Nodes []*Node
-	Edges []*Edge
-}
-
-func (g *Graph) ContainsNode(n string) bool {
-	for _, node := range g.Nodes {
-		if node.Label == n {
-			return true
-		}
+func copyVisited(visited map[string]bool) map[string]bool {
+	newVisited := make(map[string]bool)
+	for k, v := range visited {
+		newVisited[k] = v
 	}
-
-	return false
+	return newVisited
 }
 
-type Node struct {
-	Label string
-	Edges []*Edge
+type Graph struct {
+	nodes map[string]bool
+	edges map[string][]*Edge
 }
 
 type Edge struct {
-	Left     *Node
-	Right    *Node
-	Distance int
+	left     string
+	right    string
+	distance int
 }
