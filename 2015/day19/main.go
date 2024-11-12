4ee176c040069c8ca3b6ecb7d6adfdf4d39b9f8a
Nick Anderson <njcanderson@gmail.com>
2024-11-12 14:55:00 -0500

    Add day 19 2015 part 2 solution

--- 2015/day19/main.go
+++ 2015/day19/main.go
@@ -3,12 +3,18 @@ package main
 import (
 	"adventofcode/utilities"
 	"fmt"
+	"sort"
 	"strings"
 )
 
 var molecule = "ORnPBPMgArCaCaCaSiThCaCaSiThCaCaPBSiRnFArRnFArCaCaSiThCaCaSiThCaCaCaCaCaCaSiRnFYFArSiRnMgArCaSiRnPTiTiBFYPBFArSiRnCaSiRnTiRnFArSiAlArPTiBPTiRnCaSiAlArCaPTiTiBPMgYFArPTiRnFArSiRnCaCaFArRnCaFArCaSiRnSiRnMgArFYCaSiRnMgArCaCaSiThPRnFArPBCaSiRnMgArCaCaSiThCaSiRnTiMgArFArSiThSiThCaCaSiRnMgArCaCaSiRnFArTiBPTiRnCaSiAlArCaPTiRnFArPBPBCaCaSiThCaPBSiThPRnFArSiThCaSiThCaSiThCaPTiBSiRnFYFArCaCaPRnFArPBCaCaPBSiRnTiRnFArCaPRnFArSiRnCaCaCaSiThCaRnCaFArYCaSiRnFArBCaCaCaSiThFArPBFArCaSiRnFArRnCaCaCaFArSiRnFArTiRnPMgArF"
 
 func main() {
+	partOne()
+	partTwo()
+}
+
+func partOne() {
 	input := utilities.ReadInputFileToLines("2015/day19/input.txt")
 
 	instructions := getInstructions(input)
@@ -28,6 +34,64 @@ func main() {
 	}
 
 	fmt.Println(len(distinctResults))
+}
+
+// Essentially for part 2 we want to start with our molecule and work our way backwards
+// instead of starting with 'e' and working forwards. This will let us greedily iterate
+// over the 1000 shortest options each iteration instead of having to explore every single
+// option if we were expanding instead of contracting
+func partTwo() {
+	input := utilities.ReadInputFileToLines("2015/day19/input.txt")
+
+	instructions := getInstructions(input)
+
+	fmt.Println(stepsUntilE(molecule, instructions))
+}
+
+func stepsUntilE(start string, instructions []instruction) int {
+	set := []string{start}
+
+	iterations := 0
+
+	for {
+		distinctResults := make(map[string]bool)
+
+		for _, attempt := range set {
+			for _, instruction := range instructions {
+				indices := utilities.GetAllIndices(attempt, instruction.to)
+
+				window := len(instruction.to)
+
+				for i := len(indices) - 1; i >= 0; i-- {
+					index := indices[i]
+
+					transformation := attempt[:index] + instruction.from + attempt[index+window:]
+
+					distinctResults[transformation] = true
+				}
+			}
+		}
+
+		allOptions := utilities.GetMapKeys(distinctResults)
+
+		sort.Slice(allOptions, func(i, j int) bool {
+			return len(allOptions[i]) < len(allOptions[j])
+		})
+
+		if len(allOptions) > 1000 {
+			set = allOptions[:1000]
+		} else {
+			set = allOptions
+		}
+
+		iterations++
+
+		for _, attempt := range set {
+			if attempt == "e" {
+				return iterations
+			}
+		}
+	}
 }
 
 func getInstructions(input []string) []instruction {
