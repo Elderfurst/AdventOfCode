3925d5dfb02148848ceaea96c28407aacf18b36e
Nick Anderson <njcanderson@gmail.com>
2024-11-12 16:03:49 -0500

    Add day 20 2015 solutions

--- 2015/day20/main.go
+++ 2015/day20/main.go
@@ -1,1 +1,76 @@
 package main
+
+import (
+	"adventofcode/utilities"
+	"fmt"
+	"math"
+	"slices"
+)
+
+var input = 36000000
+
+func main() {
+	partOne()
+	partTwo()
+}
+
+func partOne() {
+	house := 0
+	gifts := 0
+
+	for gifts < input {
+		house++
+
+		gifts = getGiftCount(house)
+	}
+
+	fmt.Println(house)
+}
+
+func partTwo() {
+	elf := 0
+	presents := make(map[int]int)
+	houses := make(map[int]bool)
+
+	// idk loop long enough to check enough houses I guess?
+	for len(houses) < 1000000 {
+		elf++
+
+		for i := 1; i <= 50; i++ {
+			presents[i*elf] += elf * 11
+
+			if presents[i*elf] >= input {
+				houses[i*elf] = true
+			}
+		}
+	}
+
+	houseNumbers := utilities.GetMapKeys(houses)
+	slices.Sort(houseNumbers)
+
+	fmt.Println(houseNumbers[0])
+}
+func getGiftCount(house int) int {
+	divisors := make([]int, 0)
+
+	converted := int(math.Sqrt(float64(house)))
+
+	for i := 1; i <= converted; i++ {
+		if house%i == 0 {
+			// Only add duplicate divisors once
+			if house/i == i {
+				divisors = append(divisors, i)
+			} else {
+				divisors = append(divisors, i, house/i)
+			}
+		}
+	}
+
+	gifts := 0
+
+	for _, divisor := range divisors {
+		gifts += divisor * 10
+	}
+
+	return gifts
+}
